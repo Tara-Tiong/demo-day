153b49f0e789f7463599f2ab0ef3c14bd1e37124
Mission Bit <missionbit@44a-MissionBit.local>
2019-07-15 10:02:21 -0700

    commit

--- qnine.js
+++ qnine.js
@@ -1,32 +1,17 @@
-alert('Welcome to Dog Quiz');
-const question = "Which dog breed originated from?"
-const answer = prompt(question)
-alert('You answered'+ answer);
-const quiz = [
-    ["How much better is a dogs sense of smell than humans?","10,000 to 100,000"],
-    ["What type of eaters are dogs?","Omnivores"],
-    ["Which dog breed originated from Alaska?","Malamute"],
-];
+//var input = document.getElementById('myInput');
 
-function start(quiz){
-let score = 0
-//main game loop
-for(const [question,answer] of quiz){
-    const response = prompt(question);
-    if(response == answer){
-        alert('Correct!');
-        score++;
-    } else {
-        alert('Wrong! The correct answer was ' + answer);
-    }
-}
-//end of game loop
-
-gameOver();
-//function declarations 
-function ask(question){
-    return prompt(question);
-}
-
-function check(response,answer)
-//at the end of game, you reveal the player's score
+//if(input.value.length == 0)
+ 
+var userInput =
+document.querySelector('#check').onclick = function() {
+     if (right) {
+         window.location.replace('qten.html');
+     } else {
+         window.location.replace('nawww.html');
+     }
+ }
+//document.querySelector('#right').onclick = function() {
+   // console.log('working')
+   // window.location.replace('https://www.youtube.com/watch?v=ZEeckZM-z84');
+//}
+  //a.href

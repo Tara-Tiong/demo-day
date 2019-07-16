cfc7516387eb1b612e4e0337846a1c44c1b098d8
Tara-Tiong <tarationg@gmail.com>
2019-07-15 22:03:39 -0700

    it's so cooooolllll

--- main.js
+++ main.js
@@ -10,8 +10,14 @@ var toggledMenu = menuToggle.addEventListener("click", function(){
      }
 }
 )
+
+
+
+
+
+
 // this is for the typewriter
-const Typewriter = function(txtElement,words,wait = 3000){
+const Typewriter = function(txtElement,words,wait = 2000){
      this.txtElement =txtElement;
      this.words = words;
      this.txt = '';
@@ -24,27 +30,48 @@ const Typewriter = function(txtElement,words,wait = 3000){
 // type method
 Typewriter.prototype.type= function(){
 
-     // current index of word
-     const current= this.wordIndex % this.words.length;
-     // get full text of current word
-     const fulltxt = this.words[current];
+    // current index of word
+    const current= this.wordIndex % this.words.length;
+    // get full text of current word
+    const fullTxt = this.words[current];
+   
+
+    // check if deleting
+    if(this.isDeleting){
+         // remove char
+         this.txt = fullTxt.substring(0, this.txt.length - 1);
+
+    } else{
+         // add char
+         this.txt = fullTxt.substring(0, this.txt.length + 1);
+
+    }
+
+    // insert txt into element
+    this.txtElement.innerHTML = `<span class="txt"> ${this.txt} </span>`; 
+
+    // initial type speed
+    let typeSpeed = 300;
     
+    if(this.isDeleting){
+        typeSpeed /=3;
+    }
 
-     // check if deleting
-     if(this.isDeleting){
-          // remove char
-          this.txt = fullTxt.substring(0.this.txt.length + 1);
 
-     } else{
-          // add char
-          this.txt = fullTxt.substring(0.this.txt.length + 1);
-
-     }
-     // insert txt into element
-     this.txtElement.innerHTML = '<span class="txt">'this.txt'</span>'; 
-     // change
-
-     setTimeout(() => this.type(), 500);
+// if word is complete
+    if(!this.isDeleting && this.txt === fullTxt){
+        // make pause at end
+        typeSpeed = this.wait;
+        // set delete to true
+        this.isDeleting = true;
+    } else if(this.isDeleting && this.txt ==='') {
+        this.isDeleting = false;
+        // move to next word
+        this.wordIndex++;
+        // pause before start typing
+        typeSpeed = 500;
+    }
+       setTimeout(() => this.type(), typeSpeed);
 }
 
 //  init on DOM load
@@ -52,9 +79,13 @@ document.addEventListener('DOMContentLoaded',init);
 
 // init app
 function init(){
-     const txtElement = document.querySelector('.txt-type');
-     const words = JSON.parse(txtElement.getAttribute('data-words'));
-     const wait = txtElement.getAttribute('data-wait');
-     // init typewriter
-     new Typewriter(txtElement,words,wait);
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+    // init typewriter
+    new Typewriter(txtElement,words,wait);
 }
+
+
+
+

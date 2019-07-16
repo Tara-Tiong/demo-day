f866f060c05b3118b7d8e7d4922d0a9898a07de8
Fatimah520 <missionbit@38a-MissionBit.local>
2019-07-16 10:07:18 -0700

    commit

--- fati.js
+++ fati.js
@@ -70,3 +70,4 @@ function showQuestions(questions, quizContainer){
 	// finally combine our output list into one string of html and put it on the page
 	quizContainer.innerHTML = output.join('');
 }
+showQuestions(questions, quizContainer);

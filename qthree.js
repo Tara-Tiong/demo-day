b481e57cb5828b512b61af2b92f75a1371ff851e
Mission Bit <missionbit@44a-missionbit.attlocal.net>
2019-07-16 20:35:44 -0700

    commit

--- qthree.js
+++ qthree.js
@@ -1,14 +1,15 @@
 var input = document.getElementById('myInput');
 
-if(input.value.length == 0)
+// if(input.value.length == 0)
 
 document.querySelector('#test').onclick = function() {
     var userIn = document.querySelector('#myInput').value;
-
-    if (userIn == ec_ni83$^+jew_yes__) {
+    var right = 'ec_ni83$^+jew_yes__'
+    if (userIn == right) {
+        console.log('right')
         window.location.replace('qsix.html');
-    } if (userIn != ec_ni83$^+jew_yes__) {
-        // document.querySelector('h1').innerHTML = 'You are Wrong!!!!!!';
+    } else {
+        document.querySelector('h1').innerHTML = 'You are Wrong!!!!!!';
         window.location.replace('incorrect.html');
     }
 

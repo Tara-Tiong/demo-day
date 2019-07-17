bec24b57020a2031d4ee91164d8ba4d792a7eab4
Tara-Tiong <tarationg@gmail.com>
2019-07-17 15:20:57 -0700

    sfbbf

--- quizdog.js
+++ quizdog.js
@@ -25,31 +25,26 @@ function setNextQuestion() {
   showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    if (answer.correct) {
-      button.dataset.correct = answer.correct
-      //if sessionStorage.getItem('points') = 'null'
-      sessionStorage.setItem('points', 1)
-    } else {
-     var points = sessionStorage.getItem("points")
-     points= Number(points);
-     points += 1
-     sessionStorage.setItem('points', points)
-      }
-      //else get item
-      //convert iem to number
-      // add one to item
-    
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
-}
-
+function showQuestion(question)
+ {   questionElement.innerText = question.question 
+    question.answers.forEach(answer => {  
+   const button = document.createElement('button')   
+     button.innerText = answer.text 
+    button.classList.add('btn')  
+  if (answer.correct) {  
+     button.dataset.correct = answer.correct  
+     // if sessionStorage.getItem('points') = 'null'
+    sessionStorage.setItem('points', 1)     
+} else {
+  var points = sessionStorage.getItem("points") 
+    points= Number(points);    
+      points += 1    
+        sessionStorage.setItem('points', points)       }  
+             //else get item    
+                //convert iem to number   
+                    // add one to item     
+      button.addEventListener('click', selectAnswer)    
+   answerButtonsElement.appendChild(button)}) } 
 
 function resetState() {
   clearStatusClass(document.body)
@@ -69,8 +64,6 @@ function selectAnswer(e) {
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove('hide')
   } else {
-    document.getElementById('points').innerHTML = sessionStorage.getItem('points')
-
     startButton.innerText = 'Restart'
     startButton.classList.remove('hide')
   }
@@ -97,7 +90,7 @@ const questions = [
       { text: 'Akita', correct: false },
       { text: 'Malamute', correct: true },
       { text: 'Golden Retriever', correct: false },
-      { text: 'Pomeranian', correct: false },
+      { text: 'Pomeranian', correct: false }
     ]
   },
   {
@@ -106,7 +99,7 @@ const questions = [
       { text: '5,000 to 10,000 times', correct: false },
       { text: '1,000 to 5,000 times', correct: false},
       { text: '10,000 to 100,000 times', correct: true },
-      { text: '100,000 to 200,000 times', correct: false },
+      { text: '100,000 to 200,000 times', correct: false }
     ]
   },
   {
@@ -115,7 +108,7 @@ const questions = [
       { text: 'Carnivore', correct: false },
       { text: 'Herbivore', correct: false },
       { text: 'Omnivore', correct: true},
-      { text: 'IDK', correct: false },
+      { text: 'IDK', correct: false }
     ]
   },
   {
@@ -124,7 +117,7 @@ const questions = [
       { text: '340+', correct: true },
       { text: '200', correct: false },
       { text: '150', correct: false },    
-      { text: '100', correct: false },
+      { text: '100', correct: false }
 
     ]
   },
@@ -134,7 +127,7 @@ const questions = [
       { text: 'Every 1 hour', correct: false },
       { text: 'Every 2 hours', correct: true},
       { text: 'Once a day', correct: false },   
-      { text: 'Never', correct: false },
+      { text: 'Never', correct: false }
 
     ]
   },
@@ -144,7 +137,7 @@ const questions = [
       { text: 'Avocado Seed', correct: true },
       { text: 'Peanut butter', correct: false },
       { text: 'Chocolate', correct: true },     
-      { text: 'Dog Food', correct: false },
+      { text: 'Dog Food', correct: false }
 
     ]
   },
@@ -152,7 +145,7 @@ const questions = [
     question: 'Can dogs get jealous when their owner pets another dog?',
     answers: [
       { text: 'Yes', correct: true },
-      { text: 'No', correct: false },
+      { text: 'No', correct: false }
 
     ]
   },
@@ -162,7 +155,7 @@ const questions = [
       { text: 'To fight badgers', correct: true },
       { text: 'To eat your food', correct: false },
       { text: 'Be annoying', correct: false },     
-      { text: 'Peeing in your lawn', correct: false },
+      { text: 'Peeing in your lawn', correct: false }
 
     ]
   }
